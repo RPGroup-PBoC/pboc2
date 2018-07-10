c0a28ab0d9b2926581e28fce4bb363ab8ecd89e6
Griffin Chure <gchure@caltech.edu>
2018-07-09 17:45:54 -0700

    First push of all website material

---
+++ _posts/2012-12-25-resources.md
@@ -0,0 +1,8 @@
+---
+category: Stuff
+path: '/stuff'
+title: 'Other Resources'
+layout: nil
+---
+
+* [Hints to Problems](assets/PBoC2_Problems_Hints02-08-13.pdf)
--- assets.js
+++ assets.js
@@ -9627,38 +9627,38 @@ $.expr[':'].Contains = function(a,i,m){
  * Initialize a filterable list.
  */
 function Filter(list) {
-    this.el = list;
+    // this.el = list;
     
-    // Filter input
-    var form = $('<form>').attr({ 'action':'#' });
-    var input = $('<input>').attr({ 'type':'text', 'placeholder':'Filter by keyword' });
-    $(form).append(input).prependTo(this.el);
+    // // Filter input
+    // var form = $('<form>').attr({ 'action':'#' });
+    // var input = $('<input>').attr({ 'type':'text', 'placeholder':'Filter by keyword' });
+    // $(form).append(input).prependTo(this.el);
     
-    // Filter function
-    var self = this;
-    $(input).change(function () {
-        var filter = $(this).val();
-        if(filter) {
-            $(self.el).find('a:not(:Contains(' + filter + '))').parent().hide();
-            $(self.el).find('a:Contains(' + filter + ')').parent().show();
-        } else {
-            $(self.el).find('li').show();
-        }
+    // // Filter function
+    // var self = this;
+    // $(input).change(function () {
+    //     var filter = $(this).val();
+    //     if(filter) {
+    //         $(self.el).find('a:not(:Contains(' + filter + '))').parent().hide();
+    //         $(self.el).find('a:Contains(' + filter + ')').parent().show();
+    //     } else {
+    //         $(self.el).find('li').show();
+    //     }
         
-        // Hide titles when group is empty
-        $(self.el).find('ul').each(function () {
-            if (!$(this).find('li:visible').length) {
-                $(this).prev('h2').hide();
-            } else {
-                $(this).prev('h2').show();
-            }
-        });
+    //     // Hide titles when group is empty
+    //     $(self.el).find('ul').each(function () {
+    //         if (!$(this).find('li:visible').length) {
+    //             $(this).prev('h2').hide();
+    //         } else {
+    //             $(this).prev('h2').show();
+    //         }
+    //     });
 
-        return false;
-    })
-    .keyup( function () { $(this).change(); });
+    //     return false;
+    // })
+    // .keyup( function () { $(this).change(); });
     
-    return this;
+    // return this;
 }
 });
 require.register("boot/index.js", function(exports, require, module){
